c56be92a8e6cba5938c1d18eebc6017b80c6731c
Danh Nguyen <filler012@gmail.com>
2015-03-10 21:47:38 +0000

    investigate null reviews

--- search.js
+++ search.js
@@ -3,58 +3,6 @@ var cheerio = require('cheerio');
 
 
 
-function demo1(){
-
-
-(function(){
-    search();
-}())
-
-
-function search(){
-  var max = 20;
-  var offset = 0;
-  while(offset <= max){
-      getPage(undefined,offset);
-      offset+= 20;
-  }
- 
-}
-
-
-function getPage(fn,offset){
-  var url = "http://www.ratemyprofessors.com/search.jsp?query=university+of+minnesota+twin+cities&queryoption=HEADER&stateselect=&country=&dept=&queryBy=&facetSearch=&schoolName=&offset="+offset+"&max=20"
-  needle.get(url, function(error, response) {
-  if (!error && response.statusCode == 200){
-     $ = cheerio.load(response.body);
-     $("li[class='listing PROFESSOR']").each(function(i, elem) {
-        var profPage = $(this).children("a").attr('href')
-        var profName = $(this).find('.main').text();
-        needle.get("http://www.ratemyprofessors.com"+profPage, function(error, response) {
-            if (!error && response.statusCode == 200){
-                 $ = cheerio.load(response.body);
-                 console.log(profName);
-                 console.log($(".grade").first().text())
-            }
-            else{
-              console.log("ERROR")
-              console.log(error)
-              console.log(response.statusCode)
-          }
-        })
-     });
-  }
-  else{
-      console.log("ERROR")
-      console.log(error)
-      console.log(response.statusCode)
-  }
-});
-}
-    
-}
-
-
 function demo2(){
     var http = require('http');
     http.globalAgent.maxSockets = 1000;
@@ -83,7 +31,7 @@ function demo2(){
     }
     
    function search(fn){
-      var max = 20;
+      var max = 0;
       var offset = 0;
       var finished = 0;
       while(offset <= max){
@@ -103,11 +51,16 @@ function demo2(){
                 
                 getPage(function(body){
                     $ = cheerio.load(body);
-                    var grade = getGrade($)
+                    console.log(profName);
+                    var avgGrade = getGrade($);
+                    var rating = getRating($);
                     var classes = getClasses($);
-                    var prof = createProf(profName,grade,classes);
-                    var review = getScores($);
+                    var scores = getScores($);
+                    var reviews = getReviews($);
+                    var metrics = {"rating":rating,"avgGrade":avgGrade,"scores":scores};
+                    var prof = createProf(profName,classes,metrics,reviews);
                     addProf(prof,table)
+                    console.log(prof);
                 },profOptions);
              })
         },
@@ -126,6 +79,10 @@ function demo2(){
    }
    
    function getGrade($){
+       //var grade = $(".grade").slice(1).text();
+       //return grade;
+   }
+   function getRating($){
        var grade = $(".grade").first().text();
        return grade;
    }
@@ -139,42 +96,59 @@ function demo2(){
            scores.push(label + " " + score);
            console.log(label + " " + score);
        });
+       
+       return scores
    }
    
     function getReviews(){
         var reviews = $(".tftable").find("tr").slice(1);
-        if(reviews.length == 0)
+        if(reviews.length == 0){
+            console.log("quitting")
             return;
-        var counter = 3;
+        }
+        var count = 3;
         var i = 0;
         var size=  reviews.length;
         
         var reviewsTable = {}
-        while(counter && reviews == i){
+        console.log(count);
+        console.log(reviews.length);
+        while(count > 0 && reviews.length != i){
             var query = $(reviews[i])
             var _class = query.find(".name").find(".response").text().toUpperCase().replace(/ /,'','g');
             // Check if already been reviewed
             if(reviewsTable[_class] == undefined){
-                counter--;
+                var review = {};
+                var date = query.find(".date").text();
+                count--;
                 var textbook = query.find(".textbook-use").text();
-                var avg;
+                var avg = 0;
                 query.find(".score").each(function(i,ele){
-                    avg += parseInt($(this).text());
-                    count++;
-                })
-                avg = avg/3;
-                var review = query.find(".comments").find("p").text()
+                    var score = parseInt($(this).text());
+                    avg+= score;
+                });
+                avg = (avg/3).toFixed(2);
+                var reviewText = query.find(".comments").find("p").text()
+                review["date"] =  date;
+                review["textbook"] = textbook;
+                review["score"] = avg;
+                review["review"] = reviewText;
+                reviewsTable[_class] = review;
+            }else{
+                console.log("reject");
             }
+            i++;
         }
+        return reviewsTable;
    }
    
-   
   
-   function createProf(name,grade,classes){
+   function createProf(name,classes,metrics,reviews){
        return {
            "name":name,
-           "grade":grade,
-           "classes":classes
+           "metrics":metrics,
+           "classes":classes,
+           "reviews":reviews
        }
    }
    
@@ -185,7 +159,32 @@ function demo2(){
            table[prof.name].push(prof)
        }
    }
-   search();
+   
+   function searchProfessor(){
+      var options = {
+          host: 'www.ratemyprofessors.com',
+          path: '/ShowRatings.jsp?tid=897321'
+        };
+
+        getPage(function(body){
+            $ = cheerio.load(body);
+            var avgGrade = getGrade($);
+            var rating = getRating($);
+            var classes = getClasses($);
+            var scores = getScores($);
+            var reviews = getReviews($);
+            var metrics = {"rating":rating,"avgGrade":avgGrade,"scores":scores};
+            var prof = createProf("Thomas Holmes",classes,metrics,reviews);
+            addProf(prof,table)
+            console.log(prof);
+        },options);
+   }
+   //search();
+   searchProfessor();
 }
 
 demo2()
+
+
+
+
