98dd79f2a66d3151bb4dbf90ef74f80ed8df816e
Danh Nguyen <filler012@gmail.com>
2015-03-11 22:03:59 +0000

    cleaned up

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ var links = document.getElementsByTagName("a");
 var size = links.length;
 var pattern  = /http:\/\/www\.umn\.edu\/lookup\?/;
 var MATCH = 0;
-
 for(var i =0; i<size; i++){
 	if(links[i].href.search(pattern) == MATCH){
 		var node = createNode();
@@ -16,27 +15,40 @@ function createNode(){
 	var wrapper = document.createElement("div");
 	var node = document.createElement("img");
 	wrapper.appendChild(node);
+	wrapper.style.display = "inline";
+	wrapper.className = "profWrapper"
 	node.className = "prof";
-	node.style.display = "inline";
 	node.setAttribute("width","20px");
 	node.setAttribute("height","20px");
 	node.src ="http://icons.iconarchive.com/icons/wineass/ios7-redesign/512/Sample-icon.png"
 
 	wrapper.addEventListener("click",function(e){
-		console.log("execute");
-		var box = document.createElement("div");
-		box.className = "profBox";
-		e.target.appendChild(box);
+		console.log(e.target.clicked);
+		if(e.target.clicked == undefined){
+			var box = document.createElement("span");
+			box.className = "profBox";
+			box.style.display = "inline";
+			box.setAttribute("style","position:absolute;\
+			height:200px;width:300px;background:black");
+			e.target.clicked = true;
+			e.target.parentElement.appendChild(box);
+		}else{
+			e.target.clicked = undefined;
+			removeNode(e)
+		}
 	},true);
-	
-	wrapper.addEventListener("mouseout",function(e){
-		var target = e.target;
-		if(target.className != "profBox")
-			return;
-		var parent = target.parentElement;
-		parent.removeChild(parent.childNodes[0]); 
-	},true);
-	
 	return wrapper;
 }
 
+
+function removeNode(e){
+	var target = e.target;
+	var parent = target.parentElement;
+	if(parent.childNodes.length > 1)
+		parent.removeChild(parent.childNodes[1]); 
+}
+
+
+function createInfoNode(){
+	
+}
