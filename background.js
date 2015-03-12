e4097143280ed63465ea6f62021159a039c8c10f
Danh Nguyen <filler012@gmail.com>
2015-03-12 17:23:22 +0000

    made the extension talk back, untested

--- background.js
+++ background.js
@@ -1,13 +1,28 @@
-// Called when the user clicks on the browser action.
+var professors = {};
 
-// Browser Action = chrome button
-// Passes the Active tab
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  // Like exec
-  chrome.tabs.executeScript({
-	  // JS code to execute
-    code: 'document.body.style.backgroundColor="red"'
-  });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.msg == "ratemyprofessor"){
+        var prof = getProfessor(request);
+        sendResponse({prof: prof});
+    }
 });
+
+
+function getProfessor(request){
+  var profName = request.profName.replace(/PHD\./,"").replace(/, /,",");
+  var profClass = request.profClass;
+  var prof = professors[profName];
+  if(prof == undefined)
+    return;
+  if(prof.length > 1){
+    // Get one that teaches that classes if their are people with the same names
+    for(var i=0; i<prof.length; i++){
+      if(prof[i].classes[profClass] != undefined){
+        return prof[i];
+      }
+    }
+  }
+  return prof[0];
+}
