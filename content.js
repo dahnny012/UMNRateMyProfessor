fd23e26d22277063b2d8d20f70d6a0b17e001ee4
dahnny012 <nguy1952@umn.edu>
2015-03-16 11:07:10 -0500

    encoding needs help

--- content.js
+++ content.js
@@ -2,12 +2,13 @@ var links = document.getElementsByTagName("a");
 var size = links.length;
 var pattern  = /http:\/\/www\.umn\.edu\/lookup\?/;
 var MATCH = 0;
-var regex = /( [A-z]+\.?)/g
+var regex = /( [A-z]+\.?)/g;
+var classRegex = /([A-Z]+(.)+[0-9]+)/;
 var testProf = {"name":"Driessen, Michelle","metrics":{"rating":"4.0","avgGrade":"B-","scores":["Helpfulness 4.0","Clarity 4.0","Easiness 2.8"]},"classes":{"1061":"1061","10 62":"1062","CHEM1061":"CHEM1061","CHEM1021":"CHEM1021","CHEM1015":"CHEM1015","CHEM1065":"CHEM1065","CHEM1062":"CHEM1062","CHEMISTY":"CHEMISTY","CHEM1601":"CHE M1601"},"reviews":{"1061":{"class":"1061","date":" 02/17/2015","textbook":"Textbook Use: What textbook?","score":"4.33","review":" \r\n\t She' s the only teacher that I've been able to pass chemistry with. She is amazing and I would take her class any day. \r\n\t "},"CHEM 1061":{"class" :"CHEM 1061","date":" 01/15/2015","textbook":"Textbook Use: What textbook?","score":"3.33","review":" \r\n\t Her videos are nice and easy to u nderstand, but during her actual lectures she does not teach at all. You just do homework for an hour with a small group. In my opinion she thinks too highly of herself. \r\n\t "},"CHEM 1021":{"class":"CHEM 1021","date":" 12/04/2014","textbook":"Textbook Use: Essential to passing","score":"3.33","review":" \r\n\t Driessen does her best to explain concepts clearly and I thought she did a good job of it. She definitely wants you to succeed an d if you go talk to her outside of class, she's really helpful. I definitely recommend her as a prof! \r\n\t "}},"link":"www.ratemyprofessors.com /ShowRatings.jsp?tid=260962"};
-
 
 for(var i =0; i<size; i++){
 	if(links[i].href.search(pattern) == MATCH){
+		var className = getClassName(links[i]);
 		var profName = links[i].text.replace(regex,"").replace(",",", ");
 		var node = createNode(profName);
 		var target = links[i];
@@ -82,6 +83,21 @@ function reviewsHandler(e){
     }
 }
 
+function getClassName(node){
+	var parent = node;
+	for(var i=0; i<5; i++){
+		parent = parent.parentNode;
+	}
+	if(i==5){
+		parent = parent.childNodes[5].childNodes[1].data;
+		parent = parent.substr(classRegex);
+		console.log(parent);
+	}else{
+		return "";
+	}
+	return parent
+}
+
 function createInfoNode(response){
 	var prof = response.prof;
 	var profBox = createDiv("profBox");
@@ -145,3 +161,5 @@ function createInfoNode(response){
 	}
 	return profBox;
 }
+
+
