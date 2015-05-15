df803ca791715f76d8fd342b298c430dc589b0bd
unknown <nguy1952@HOOKE.CSE.UMN.EDU>
2015-05-14 21:29:32 -0500

    i dont know

--- app/parse.js
+++ app/parse.js
@@ -1,3 +1,5 @@
+var icon = "http://icons.iconarchive.com/icons/wineass/ios7-redesign/512/Sample-icon.png";
+
 function Parse(){
 	this.iframe = document.getElementsByTagName("iframe")[0].
 		contentWindow.document;
@@ -8,7 +10,17 @@ function Parse(){
 
 
 Parse.prototype.run = function(){
-	this.findProfessors();
+	this.findProfessors(addButton);
+	
+	function addButton(node){
+			var image = new Image();
+			image.src = icon;
+			image.style.width = "20px";
+			image.style.height = "20px";
+			image.className = "prof"; 
+			node.appendChild(image);
+			image.addEventListener("click",openRating);
+	}
 };
 
 
@@ -25,22 +37,46 @@ Parse.prototype.classTable = function(){
 };
 
 
-Parse.prototype.findProfessors = function(){
+Parse.prototype.findProfessors = function(cb){
 	var classes = this.classTable();
 	if(classes == null){
 		return null;
 	}
 	var table = $(classes).find(".PSLEVEL3GRIDROW");
-	var professors = table.filter(professorFilter);
-
-};
-
-function professorFilter(node){
-	var profNode = $(node).find("span");
-	if(profNode.id.indexOf("MTG_INSTR") < 0){
-		return;
+	var size = table.length;
+	for(var i=0; i<size; i++){
+		var profNode = professorFilter(table[i]);
+		if(profNode){
+			cb(profNode);
+		}
 	}
+	
+	function professorFilter(node){
+		var profNode = $(node).find("span")[0];
+		
+		if(profNode === undefined || profNode.id.indexOf("MTG_INSTR") < 0){
+			return false;
+		}
+		return profNode;
+	};
 };
+
+
+
+
+function openRating(e){
+	if(e.target.className === "prof"){
+			var node = createInfoNode(prof);
+	}
+}
+
+function sendMsg(name,_class,target){
+	chrome.runtime.sendMessage({msg: "ratemyprofessor",profName:name,profClass: _class},
+	function(response){
+		var node = 	createInfoNode(response,name);
+		target.appendChild(node);
+	});
+}
 
 chrome.runtime.onMessage.addListener(
 	function(request,sender,sendResponse){
