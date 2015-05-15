63508cc1378ba98b00e09b5f3fed940c7ffd7516
Danh Nguyen <nguy1952@umn.edu>
2015-05-14 19:53:55 -0500

    what is life what is love

--- app/parse.js
+++ app/parse.js
@@ -1,20 +1,24 @@
 function Parse(){
 	this.iframe = document.getElementsByTagName("iframe")[0].
 		contentWindow.document;
+	if(this.iframe == null){
+		console.log("Cant find iframe");
+	}
 }
 
 
 Parse.prototype.run = function(){
 	this.findProfessors();
-}
+};
 
 
 
 Parse.prototype.classTable = function(){
 	// Look at Iframe
 	// By some magic metric conclude were at classes
-	var table = this.iframe.getElementByID("win0divDERIVED_CLSRCH_GROUP6");
+	var table = this.iframe.getElementById("win0divDERIVED_CLSRCH_GROUP6");
 	if(table == null){
+		console.log("Class table not found");
 		return null;
 	}
 	return table;
@@ -23,15 +27,25 @@ Parse.prototype.classTable = function(){
 
 Parse.prototype.findProfessors = function(){
 	var classes = this.classTable();
-	if(classes == null)
+	if(classes == null){
 		return null;
-	var professors = $(classes).find("th[abbr='Instructor']");
-	console.log(professors);
+	}
+	var table = $(classes).find(".PSLEVEL3GRIDROW");
+	var professors = table.filter(professorFilter);
+
+};
+
+function professorFilter(node){
+	var profNode = $(node).find("span");
+	if(profNode.id.indexOf("MTG_INSTR") < 0){
+		return;
+	}
 };
 
 chrome.runtime.onMessage.addListener(
 	function(request,sender,sendResponse){
-		console.log("Curse the world");
+		var parse = new Parse();
+		parse.run();
 	}
 );
 
